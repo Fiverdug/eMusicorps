7e911614af1ead938c2d9663ebaa89f4bcc680f2
Amedeo123 <amedeo.ceglia@umontreal.ca>
2021-08-09 11:45:05 -0400

    add export to mat file

--- read_data.py
+++ read_data.py
@@ -1,6 +1,9 @@
 import data_processing
+import scipy.io as sio
 
 # Read pickle file saved with RT_process_pytrigno:
-filename = "data_stream_01_08_2021/stream_data_xxx"  # path of saved file
-data = data_processing.read_data(filename)
-print(data.keys())
+filename = "/home/amedeo/Documents/programmation/eMusicorps/test_accel/stream_data_xxx"  # path of saved file
+data = data_processing.read_data(filename)  # return dictionary data type
+
+# save to matlab format file:
+# sio.savemat("data_xxx.mat", data)
