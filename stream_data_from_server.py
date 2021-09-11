d6668285ac2775b26786166570c5080d9385311e
Amedeo123 <amedeo.ceglia@umontreal.ca>
2021-09-11 07:10:26 +0200

    add different pianist

--- stream_data_from_server.py
+++ stream_data_from_server.py
@@ -11,12 +11,14 @@ if __name__ == '__main__':
     read_freq = 100  # Be sure that it's the same than server read frequency
     show_data = ["raw_emg"]  # can be ["emg"] to show process EMG
     device_host = "192.168.1.211"  # IP address of computer which run trigno device
+    n_electrode = 10
+    n_elec_pianist_1 = 5
+    n_elec_pianist_2 = n_electrode - n_elec_pianist_1
 
     # load MVC data from previous trials.
     file_name = "MVC_xxxx.mat"
     file_dir = "MVC_01_08_2021"
     list_mvc = sio.loadmat(f"{file_dir}/{file_name}")["MVC_list_max"]
-    n_electrode = 5
     list_mvc = list_mvc[:, :n_electrode].T
     list_mvc = list_mvc.tolist()
 
@@ -25,7 +27,7 @@ if __name__ == '__main__':
     output_dir = "test_accel"
 
     # Run streaming data
-    # muscles_idx = (0, n_electrode - 1)
+    muscles_idx_pianist = [(0, n_elec_pianist_1 - 1), (n_elec_pianist_1 - 1, n_electrode - 1)]
     host_ip = 'localhost'
     host_port = 50000
     get_accel = True
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     print_data = True
     OSC_ip = "127.0.0.1"
     OSC_port = 51337
-    OSC_stream = True
+    OSC_stream = False
     save_data = True
     data_path = 'streamed_data'
     if os.path.isfile(data_path):
@@ -43,6 +45,11 @@ if __name__ == '__main__':
         OSC_client = SimpleUDPClient(OSC_ip, OSC_port)
         print("Streaming OSC activated")
     count = 0
+    pianist = input("Please choose a pianist (1 or 2), then press enter.")
+    pianist = int(pianist)
+    muscles_idx = slice(muscles_idx_pianist[pianist - 1][0], muscles_idx_pianist[pianist - 1][-1])
+    initial_electrode = 0 if pianist == 1 else n_elec_pianist_1
+    final_electrode = n_elec_pianist_1 if pianist == 1 else n_elec_pianist_1 + n_elec_pianist_2
     while True:
         client = Client(host_ip, host_port, "TCP")
         data = client.get_data(data=["emg", 'imu'],
@@ -56,19 +63,20 @@ if __name__ == '__main__':
 
         data['gyro_proc'] = []
         data['accel_proc'] = []
-        EMG = np.array(data['emg'])
-        raw_emg = np.array(data['raw_emg'])
+        data["pianist_player"] = pianist
+        EMG = np.array(data['emg'])[initial_electrode:final_electrode, :]
+        raw_emg = np.array(data['raw_emg'])[initial_electrode:final_electrode, :]
 
         if len(np.array(data['imu']).shape) == 3:
-            accel_proc = np.array(data['imu'])[:, :3, :]
-            gyro_proc = np.array(data['imu'])[:, 3:6, :]
-            raw_accel = np.array(data['raw_imu'])[:, :3, :]
-            raw_gyro = np.array(data['raw_imu'])[:, 3:6, :]
+            accel_proc = np.array(data['imu'])[initial_electrode:final_electrode, :3, :]
+            gyro_proc = np.array(data['imu'])[initial_electrode:final_electrode, 3:6, :]
+            raw_accel = np.array(data['raw_imu'])[initial_electrode:final_electrode, :3, :]
+            raw_gyro = np.array(data['raw_imu'])[initial_electrode:final_electrode, 3:6, :]
         else:
-            accel_proc = np.array(data['imu'])[:n_electrode, :]
-            gyro_proc = np.array(data['imu'])[n_electrode:, :]
-            raw_accel = np.array(data['raw_imu'])[:, :3, :]
-            raw_gyro = np.array(data['raw_imu'])[:, 3:6, :]
+            accel_proc = np.array(data['imu'])[initial_electrode:final_electrode, :]
+            gyro_proc = np.array(data['imu'])[n_electrode + initial_electrode:n_electrode + final_electrode:, :]
+            raw_accel = np.array(data['raw_imu'])[initial_electrode:final_electrode, :3, :]
+            raw_gyro = np.array(data['raw_imu'])[initial_electrode:final_electrode, 3:6, :]
 
         if print_data is True:
             print(f"Accel data :\n"
@@ -83,69 +91,26 @@ if __name__ == '__main__':
 
         if OSC_stream is True:
             if get_emg is True:
-                # OSC_client.send_message("/EMG/processed/", emg_proc[:, -1:].tolist())
                 EMG_list = EMG[:, -1:].reshape(EMG.shape[0])
                 EMG_list = EMG_list.tolist()
-                # float_array = EMG[:, -1:].astype(np.float)
-                # OSC_client.send_message("/EMG/processed",
-                #                         [float(float_array[0]), float(float_array[1]), float(float_array[2]),
-                #                          float(float_array[3]), float(float_array[4])])
                 OSC_client.send_message("/EMG/processed", EMG_list)
 
             if len(np.array(data['imu']).shape) == 3:
                 if get_accel is True:
-                    # OSC_client.send_message("/accel/", accel_proc[:, :, -1:].tolist())
-                    # accel_float = accel_proc[:, :, -1:].astype(np.float)
-                    # OSC_client.send_message("/accel/0",
-                    #                         [float(accel_float[0][0]), float(accel_float[0][1]), float(accel_float[0][2])])
-                    # OSC_client.send_message("/accel/1",
-                    #                         [float(accel_float[1][0]), float(accel_float[1][1]), float(accel_float[1][2])])
-                    # OSC_client.send_message("/accel/2",
-                    #                         [float(accel_float[2][0]), float(accel_float[2][1]), float(accel_float[2][2])])
-                    # OSC_client.send_message("/accel/3",
-                    #                         [float(accel_float[3][0]), float(accel_float[3][1]), float(accel_float[3][2])])
-                    # OSC_client.send_message("/accel/4",
-                    #                         [float(accel_float[4][0]), float(accel_float[4][1]), float(accel_float[4][2])])
                     for i in range(accel_proc.shape[0]):
                         accel_list = accel_proc[i, :, -1:].reshape(3)
                         accel_list = accel_list.tolist()
                         OSC_client.send_message(f"/accel/{i}", accel_list)
-
                 if get_gyro is True:
-                    # OSC_client.send_message("/gyro/", gyro_proc[:, :, -1:].tolist())
-                    # gyro_float = gyro_proc[:, :, -1:].astype(np.float)
-                    # OSC_client.send_message("/gyro/0",
-                    #                         [float(gyro_float[0][0]), float(gyro_float[0][1]), float(gyro_float[0][2])])
-                    # OSC_client.send_message("/gyro/1",
-                    #                         [float(gyro_float[1][0]), float(gyro_float[1][1]), float(gyro_float[1][2])])
-                    # OSC_client.send_message("/gyro/2",
-                    #                         [float(gyro_float[2][0]), float(gyro_float[2][1]), float(gyro_float[2][2])])
-                    # OSC_client.send_message("/gyro/3",
-                    #                         [float(gyro_float[3][0]), float(gyro_float[3][1]), float(gyro_float[3][2])])
-                    # OSC_client.send_message("/gyro/4",
-                    #                         [float(gyro_float[4][0]), float(gyro_float[4][1]), float(gyro_float[4][2])])
-
                     for i in range(gyro_proc.shape[0]):
                         gyro_list = gyro_proc[i, :, -1:].reshape(3)
                         gyro_list = gyro_list.tolist()
                         OSC_client.send_message(f"/gyro/{i}", gyro_list)
-
             else:
-                # OSC_client.send_message("/accel/processed/", emg_proc[:, -1:].tolist())
-                # accel_float = accel_proc[:, -1:].astype(np.float)
-                # OSC_client.send_message("/accel/processed",
-                #                         [float(accel_float[0]), float(accel_float[1]), float(accel_float[2]),
-                #                          float(accel_float[3]), float(accel_float[4])])
                 if get_accel is True:
                     accel_list = accel_proc[:, -1:].reshape(accel_proc.shape[0])
                     accel_list = accel_list.tolist()
                     OSC_client.send_message("/accel/processed", accel_list)
-
-                # OSC_client.send_message("/gyro/processed/", emg_proc[:, -1:].tolist())
-                # gyro_float = gyro_proc[:, -1:].astype(np.float)
-                # OSC_client.send_message("/gyro/processed",
-                #                         [float(gyro_float[0]), float(gyro_float[1]), float(gyro_float[2]),
-                #                          float(gyro_float[3]), float(gyro_float[4])])
                 if get_gyro is True:
                     gyro_list = gyro_proc[:, -1:].reshape(gyro_proc.shape[0])
                     gyro_list = gyro_list.tolist()
@@ -157,9 +122,14 @@ if __name__ == '__main__':
                 count += 1
             for key in data.keys():
                 if key == 'imu':
-                    data[key] = np.array(data[key])
-                    data['accel_proc'] = data[key][:n_electrode, :]
-                    data['gyro_proc'] = data[key][n_electrode:, :]
+                    if len(np.array(data['imu']).shape) == 3:
+                        data[key] = np.array(data[key])
+                        data['accel_proc'] = data[key][:n_electrode, :3, :]
+                        data['gyro_proc'] = data[key][n_electrode:, 3:6, :]
+                    else:
+                        data[key] = np.array(data[key])
+                        data['accel_proc'] = data[key][:n_electrode, :]
+                        data['gyro_proc'] = data[key][n_electrode:, :]
                 else:
                     data[key] = np.array(data[key])
             add_data_to_pickle(data, data_path)
